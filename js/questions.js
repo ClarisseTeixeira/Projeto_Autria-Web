dd7a7d648c6c7af6f7081e4a8d8c6f6373bd39bc
Clarisse <mclarissetc@gmail.com>
2023-01-11 20:31:30 -0300

    deumerda
    tava tudo dando certo aí eu estranhei, logo a merda apareceu

---
+++ js/questions.js
@@ -0,0 +1,51 @@
+export default[
+    {
+        question: "pergunta 1",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: false},
+            { option: "3", correct: false},
+            { option: "4", correct: false},
+        ],
+    },
+    
+    {
+        question: "pergunta 2",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: false},
+            { option: "3", correct: false},
+            { option: "4", correct: false},
+        ],
+    },
+
+    {
+        question: "pergunta 3",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: false},
+            { option: "3", correct: false},
+            { option: "4", correct: false},
+        ],
+    },
+
+    {
+        question: "pergunta 4",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: false},
+            { option: "3", correct: false},
+            { option: "4", correct: false},
+        ],
+    },
+
+    {
+        question: "pergunta 5",
+        answers: [
+            { option: "1", correct: true},
+            { option: "2", correct: false},
+            { option: "3", correct: false},
+            { option: "4", correct: false},
+        ],
+    },
+]
